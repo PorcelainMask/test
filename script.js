811340c214d7137873b54f1c3d990d34d3e1b442
PorcelainMask <151387865+PorcelainMask@users.noreply.github.com>
2023-11-19 06:19:49 +0000

    Animation Update
    Incorporated an animation between stages.

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function test(event){
 	text = "X: " + xMouse + " Y: " + yMouse;
 	document.getElementById("test").innerHTML = text;
 }
+
+function goToTargetPage(site) {
+  // Add the "hide" class to the body to trigger the fade-out animation
+  document.body.classList.add("hide");
+
+  // After a delay (0.5 seconds, matching the CSS transition duration), navigate to the target page
+  setTimeout(function () {
+    window.location.href = site;
+  }, 500); // 500 milliseconds
+}
